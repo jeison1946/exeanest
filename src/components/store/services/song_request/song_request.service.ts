c537919ec2b9ff767c70c0320830c9d3a1b2d57a
Json Jimenez <jeisonjimenes.1998@gmail.com>
2024-05-17 16:19:25 -0500

    add service song request

--- src/components/store/services/song_request/song_request.service.ts
+++ src/components/store/services/song_request/song_request.service.ts
@@ -3,6 +3,7 @@ import { SongRequesttDto } from '../../dtos/song_request/song_request.dto';
 import { SongRequest } from '../../entities/song_request/song_request.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as moment from 'moment-timezone';
 
 @Injectable()
 export class SongRequestService {
@@ -10,15 +11,31 @@ export class SongRequestService {
     @InjectModel(SongRequest.name) private readonly model: Model<SongRequest>,
   ) {}
   async create(data: SongRequesttDto) {
+    let flag = false;
     const last = await this.model
-      .findOne({ pos: data.pos, id: data.id, finish: false })
+      .findOne({ pos: data.pos, id: data.id })
       .sort({ date: 1 });
 
+    const currentDate = new Date();
+    currentDate.setUTCHours(currentDate.getUTCHours() - 5);
     if (last) {
+      if (!last.finish) {
+        flag = true;
+      } else {
+        const providedDate = new Date(last.date);
+        console.log(providedDate, currentDate);
+        const diffInMs = currentDate.getTime() - providedDate.getTime();
+        // Convertir la diferencia a minutos
+        const diffInMinutes = diffInMs / (1000 * 60);
+        if (diffInMinutes < 120) {
+          flag = true;
+        }
+      }
+    }
+
+    if (flag) {
       throw Error('Esta canción ya fue solicitada');
     } else {
-      const currentDate = new Date();
-      currentDate.setUTCHours(currentDate.getUTCHours() - 5);
       data.date = currentDate;
       const newEntity = new this.model(data);
       return await newEntity.save();
